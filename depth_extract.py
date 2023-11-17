28b1b4e2844329a91b619fe545690cea1fb53218
NevermindNilas <128264457+NevermindNilas@users.noreply.github.com>
2023-11-18 01:21:57 +0200

    added progress bar

--- depth_extract.py
+++ depth_extract.py
@@ -3,6 +3,10 @@ import cv2
 import numpy as np
 import time
 from threading import Thread
+from tqdm import tqdm
+from torch.nn import functional as F
+from queue import Queue, Empty
+
 
 class VideoDecodeStream:
     def __init__(self, video_file, width, height):
@@ -24,7 +28,6 @@ class VideoDecodeStream:
         while not self.stopped:
             grabbed, frame = self.vcap.read()
             if not grabbed:
-                print('The video buffering has been finished')
                 self.stopped = True
                 break
             frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
@@ -47,12 +50,13 @@ class VideoDecodeStream:
         return cv2.VideoWriter_fourcc(*'mp4v')
     
 class DepthScanStream:
-    def __init__(self,half, video_stream, model, device):
+    def __init__(self,half, video_stream, model, device, progressbar):
         self.depth_buffer = []
         self.half = half
         self.video_stream = video_stream
         self.model = model
         self.device = device
+        self.progressbar = progressbar
         # One frame has to be initilized otherwise the for loop will just skip it and imshow will go nutzzzz
         # This can be potentially bad on I/O bound systems, but it's the only way I found to make it work
         frame = self.video_stream.read()
@@ -81,8 +85,9 @@ class DepthScanStream:
             depth_map = prediction[0].cpu().numpy()
             depth_map = (depth_map - depth_map.min()) / (depth_map.max() - depth_map.min()) * 255
             depth_map = depth_map.astype(np.uint8)
+            self.progressbar.update(1)
             return depth_map
-        
+
     def update(self):
         while not self.stopped:
             image = self.video_stream.read()
@@ -90,78 +95,44 @@ class DepthScanStream:
                 depth_frame = self.depth_extract(image)
                 self.depth_buffer.append(depth_frame)
             elif self.video_stream.stopped == True:
-                print('The video depth scanning has been finished')
                 self.stopped = True
                 break
     
     def read(self):
         return self.depth_buffer.pop(0) if self.depth_buffer else None
-
-class VideoEncodeStream:
-    def __init__(self, video_stream, depth_stream, width, height, output_path):
-        self.video_stream = video_stream
-        self.depth_stream = depth_stream
-        self.width = width
-        self.height = height
-        self.output_path = output_path
-        self.out = cv2.VideoWriter(self.output_path, self.video_stream.get_fourcc(), self.video_stream.get_fps(), (self.width, self.height), isColor=False)
-        self.thread = Thread(target=self.update, args=())
-        self.thread.daemon = True
     
-    def start(self):
-        self.stopped = False
-        self.thread.start()
-    
-    def stop(self):
-        self.stopped = True
-    
-    def update(self):
-        while not self.stopped:
-            image = self.depth_stream.read()
-            if image is not None:
-                self.out.write(image)
-            elif self.video_stream.stopped == True and self.depth_stream.stopped == True:
-                print('The video encoding has been finished')
-                self.stopped = True
-                break
-            
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                self.stopped = True
-                break
-        cv2.destroyAllWindows()
-            
-def load_model(model_type):
+def load_model(model_type, half):
     " Had to separate load model from videodepthstream because of new users who had to download the model first could run into issues with timing "
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = torch.hub.load("intel-isl/MiDaS", model_type, pretrained=True).to(device)
     #model = torch.hub.load("facebookresearch/dinov2", model="dinov2_vitl14").to(device) WTH is this model?? WHY DO I NEED 2 MODELS TO DO ONE JOB???
+    if torch.cuda.is_available():
+        torch.backends.cudnn.enabled = True
+        torch.backends.cudnn.benchmark = True
+        if(half):
+            torch.set_default_tensor_type(torch.cuda.HalfTensor)
     model.eval()
     model.cuda()
     
     return model, device
 
 def depth_extract_video(video_file, output_path, width, height, half, nt, verbose, model_type):
-    model, device = load_model(model_type)
+    model, device = load_model(model_type, half)
     video_stream = VideoDecodeStream(video_file, width, height)
     video_stream.start()
-    depth_stream = DepthScanStream(half, video_stream, model, device)
+    progressbar = tqdm(total=video_stream.get_frame_count(), unit="frames")
+    depth_stream = DepthScanStream(half, video_stream, model, device, progressbar)
     depth_stream.start()
-    #output_stream = VideoEncodeStream(video_stream, depth_stream, width, height, output_path)
-    #output_stream.start()
-    out = cv2.VideoWriter(output_path, video_stream.get_fourcc(), video_stream.get_fps(), (width, height), isColor=False)
-    start_time = time.time()
-
-    i = 0 # for fps calculation
+    out = cv2.VideoWriter(output_path, depth_stream.video_stream.get_fourcc(), depth_stream.video_stream.get_fps(), (width, height))
     while True:
         depth_frame = depth_stream.read()
         if depth_frame is None and depth_stream.stopped == True:
             break
         elif verbose == "True" and depth_frame is not None:
-            cv2.imshow('Depth, press CTRL + C inside the terminal to exit', depth_frame)
-            i += 1
-            elapsed_time = time.time() - start_time
-            current_fps = (i + 1) / elapsed_time
-            print(f"Current FPS: {round(current_fps, 2)}", end="\r")
+            try:
+                cv2.imshow('Depth, press CTRL + C inside the terminal to exit', depth_frame)
+            except:
+                pass
             
         out.write(depth_frame)
         
@@ -169,5 +140,3 @@ def depth_extract_video(video_file, output_path, width, height, half, nt, verbos
             break
             
     cv2.destroyAllWindows()
-    total_process_time = time.time() - start_time
-    print(f"Process done in {total_process_time:.2f} seconds")
